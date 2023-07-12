666e7e1984376aa111f329a14a798b471a12a182
NT-Phong <87640997+phong-2107@users.noreply.github.com>
2023-07-12 19:42:15 +0700

    second commit

---
+++ src/config/dist/database.dev.js
@@ -0,0 +1,15 @@
+"use strict";
+
+require('dotenv').config();
+
+var mysql = require('mysql2'); // create the connection to database
+
+
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+module.exports = connection;
--- src/dist/app.dev.js
+++ src/dist/app.dev.js
@@ -8,29 +8,18 @@ var configViewEngine = require('./config/viewEngine');
 
 var webRoutes = require('./routes/web');
 
+var connection = require('./config/database');
+
 var app = express();
 var port = process.env.PORT || 8888;
-var hostname = process.env.HOST_NAME;
-
-var mysql = require('mysql2'); // config template engine
-
+var hostname = process.env.HOST_NAME; // config template engine
 
 configViewEngine(app); // config routes
 
-app.use('/test', webRoutes); // create the connection to database
-
-var connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3307,
-  user: 'root',
-  password: '123456',
-  database: 'hoidanit'
-}); // simple query
+app.use('/test', webRoutes); // simple query
 
 connection.query('SELECT * FROM Users u ', function (err, results, fields) {
   console.log("result = ", results); // results contains rows returned by server
-
-  console.log("fields = ", fields); // fields contains extra meta data about results, if available
 });
 app.listen(port, hostname, function () {
   console.log("Example app listening on port ".concat(port));
